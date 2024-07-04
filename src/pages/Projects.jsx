3f48d21701de12b16d134665841a6381cd4fe8a6
mathias barbosa <programadorpromedio@gmail.com>
2024-07-04 14:09:30 -0300

    redirect

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,7 +9,7 @@ export default function Projects() {
         <h2 className='text-white text-5xl font-pixel_digivolve '>Proyectos</h2>
         <h1 className="mt-4 text-3xl text-white">Desarrollamos páginas web, herramientas y software, <strong className="text-gray">a medida y de alta calidad.</strong></h1>
       </div>
-      <section className=' mt-16 flex flex-wrap flex-col items-center md:flex-flow justify-between gap-y-4'>
+      <section className=' mt-16 flex flex-wrap flex-col items-center md:flex-row justify-between gap-y-4'>
         {
           projects.map((p) => <CardProject key={p.id} {...p} />)
         }
