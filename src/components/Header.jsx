e9729fee17322ca6b575b9b37bb71ad6be8fa5d5
mathias barbosa <programadorpromedio@gmail.com>
2024-07-15 13:34:52 -0300

    upload btn TITLE AND H1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,8 @@ export default function Header() {
     <section className="p-8 2xl:p-12">
       <div className="text-left">
         <h2 className='text-white text-4xl lg:text-5xl font-pixel_digivolve '>Hey! soy Programador <br />Promedio.</h2>
-        <h1 className="mt-4 text-3xl text-white">Diseño y desarrollo <strong className="text-gray"> productos digitales.</strong></h1>
+        {/* <h1 className="mt-4 text-3xl text-white">Diseño y desarrollo <strong className="text-gray"> productos digitales.</strong></h1> */}
+        <h1 className="mt-4 text-3xl text-white font-pixel_digivolve ">AGENCIA DIGITAL</h1>
       </div>
       <div className="text-left mt-4 max-[505px]:flex-col  flex flex-row flex-wrap gap-4">
         <a href="https://calendly.com/programadorpromedio/30min" target="_blank" className="text-white bg-dark_btn border border-gray py-2 px-6 rounded-lg hover:opacity-50 cursor-pointer flex items-center gap-2"> <span className="icon-[bx--calendar]"></span> Consultoría gratis</a>
