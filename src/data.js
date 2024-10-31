aba8ad803ac60a1f14882fa5cc11c2ce20c7dadc
mathias barbosa <programadorpromedio@gmail.com>
2024-10-31 10:56:49 -0300

    actualizacion y version para subir a hostinger

--- src/data.js
+++ src/data.js
@@ -1,65 +1,89 @@
 export const projects = [
   {
+    id: 8,
+    title: 'Amarres y uniones',
+    description: 'Landing Page',
+    image: '/amarres.webp',
+    link: '',
+  },
+  {
+    id: 7,
+    title: 'Tienda Online',
+    description: 'Ecommerce',
+    image: '/tienda.webp',
+    link: '',
+  },
+  {
+    id: 6,
+    title: 'Pastelito Cake Shop',
+    description: 'Catalogo Web',
+    image: '/pastelito.webp',
+    link: 'https://pastelito.vercel.app/',
+  },
+  {
     id: 1,
-    title: "Colegio | Instituto Santa Rosa",
-    description: "Sitio Web Institucional",
-    image: "/santarosaV2.png",
-    link: "https://santarosachivilcoy.com/"
+    title: 'Colegio | Instituto Santa Rosa',
+    description: 'Sitio Web Institucional',
+    image: '/santarosaV2.png',
+    link: 'https://santarosachivilcoy.com/',
   },
   {
     id: 2,
-    title: "Pablo Galfré",
-    description: "Portafolio personal",
-    image: "/galfreV2.png",
-    link: "https://pablogalfre.com/"
+    title: 'Pablo Galfré',
+    description: 'Portafolio personal',
+    image: '/galfreV2.png',
+    link: 'https://pablogalfre.com/',
   },
   {
     id: 3,
-    title: "Turnero - Museo Historico Nacional",
-    description: "Software a medida",
-    image: "/museo.png",
-    link: ""
+    title: 'Turnero - Museo Historico Nacional',
+    description: 'Software a medida',
+    image: '/museo.png',
+    link: '',
   },
   {
     id: 4,
-    title: "Atlanta Audiovisual",
-    description: "Portafolio empresarial",
-    image: "/atlantaV2.png",
-    link: "https://atlantaaudiovisual.com.ar/"
+    title: 'Atlanta Audiovisual',
+    description: 'Portafolio empresarial',
+    image: '/atlantaV2.png',
+    link: 'https://atlantaaudiovisual.com.ar/',
   },
   {
     id: 5,
-    title: "Sistema de gestión de personal",
-    description: "Software a medida",
-    image: "/montelectro.png",
-    link: ""
+    title: 'Sistema de gestión de personal',
+    description: 'Software a medida',
+    image: '/montelectro.png',
+    link: '',
   },
-]
+];
 
 export const services = [
   {
     id: 1,
-    title: "💡 Análisis y planificación",
-    description: "Creamos planes a medida para alcanzar tus objetivos",
+    title: '💡 Análisis y planificación',
+    description: 'Creamos planes a medida para alcanzar tus objetivos',
   },
   {
     id: 2,
-    title: "📲 Diseño y desarrollo web",
-    description: "Desarrollamos sitios web, landing pages, blogs, portfolios. Funcionales, intuitivos y optimizados",
+    title: '📲 Diseño y desarrollo web',
+    description:
+      'Desarrollamos sitios web, landing pages, blogs, portfolios. Funcionales, intuitivos y optimizados',
   },
   {
     id: 3,
-    title: "🖥️ Software a medida",
-    description: "Desarrollamos software que se adapta a tus necesidades específicas",
+    title: '🖥️ Software a medida',
+    description:
+      'Desarrollamos software que se adapta a tus necesidades específicas',
   },
   {
     id: 4,
-    title: "🛠️ Alojamiento y mantenimiento web",
-    description: "Nos encargamos del alojamiento y soporte técnico para que tu web este siempre online",
+    title: '🛠️ Alojamiento y mantenimiento web',
+    description:
+      'Nos encargamos del alojamiento y soporte técnico para que tu web este siempre online',
   },
   {
     id: 5,
-    title: "🎨 Branding e identidad de marca",
-    description: "Te ayudamos a crear y gestionar una marca única y destacada",
+    title: '🎨 Branding e identidad de marca',
+    description: 'Te ayudamos a crear y gestionar una marca única y destacada',
   },
-]
+];
